97c9d530a0b6f2eac8d6ab88db284876422194d1
Ching-Han, Huang <tibette0817@gmail.com>
2019-06-09 23:13:28 +0800

    Add files via upload

---
+++ password_picker.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+adjectives = ["sleepy", "slow", "smelly", "wet",
+              "fat", "red", "orange", "yellow",
+              "green", "blue", "purple", "fluffy",
+              "white", "proud", "brave"]
+
+nouns = ["apple", "dinosaur", "ball", "toaster",
+         "goat", "dragon", "hammer", "duck", "panda"]
+
+print("Welcome to Password Picker!")
+
+while True:
+
+    for num in range(3):
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(0, 100)
+        special_char = random.choice(string.punctuation)
+
+        password = adjective + noun + str(number) + special_char
+
+        print("Your password is: %s" % password)
+        
+    response = input("Would you like another password? (y/n)")
+
+    if response == 'n':
+        break
